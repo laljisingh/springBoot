e35b1ccacd22d4f0ec0b66f30c7809f1d36ff69e
LAL JI SINGH <60351793+laljisingh@users.noreply.github.com>
2023-02-19 20:08:22 +0530

    Delete ecommerce directory

--- ecommerce/src/main/java/com/laljisingh/ecommerce/dao/AddresRepository.java
+++
@@ -1,8 +0,0 @@
-package com.laljisingh.ecommerce.dao;
-
-import com.laljisingh.ecommerce.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddresRepository extends JpaRepository<Address,Integer> {
-
-}
--- ecommerce/src/main/java/com/laljisingh/ecommerce/dao/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.laljisingh.ecommerce.dao;
-
-import com.laljisingh.ecommerce.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
