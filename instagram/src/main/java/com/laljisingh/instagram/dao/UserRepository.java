44ed3548bd31f6c0e5315c9cd239f649328dad4c
lalji singh <lalji7411@gmail.com>
2023-02-22 19:05:43 +0530

    project partilly added

--- instagram/src/main/java/com/laljisingh/instagram/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.laljisingh.instagram.dao;
-
-import com.laljisingh.instagram.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
